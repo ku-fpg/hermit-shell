d93b352529875a0920a9c991f8620416f5e1100a
Andy Gill <andygill@ku.edu>
2015-05-27 00:42:33 -0500

    Drafting client side of shell

--- src/HERMIT/API.hs
+++ src/HERMIT/API.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE LambdaCase, OverloadedStrings, KindSignatures, GADTs #-}
 module HERMIT.API where
-        
+
+import Data.Aeson
+import Control.Monad.Remote.JSON as JSONRPC
+
+{-        
 import HERMIT.GHCI.Session
         
 -- | redisplays the current state to STDOUT.
@@ -21,6 +26,33 @@ newtype TypedEffectH :: * -> * where
 shellEffect :: ShellEffect -> TypedEffectH
 shellEffect = print 'ShellEffectH
 
-        
 prim :: Name -> [Value] -> Value
 prim = undefined
+-}
+
+--- Main call-HERMIT function
+
+class Shell f where
+  toShell   :: f a -> Value
+  fromShell :: f a -> Value -> Maybe a
+  
+data ShellEffect :: * -> * where
+  ShellEffect :: Value -> ShellEffect ()
+
+instance Shell ShellEffect where
+  toShell (ShellEffect v) = v
+  fromShell (ShellEffect {}) _ = return ()
+  
+display :: ShellEffect ()
+display = ShellEffect $ object ["method" .= ("display" :: String)]
+
+session :: JSONRPC.Session
+session = undefined
+
+send :: Shell f => f a -> IO a
+send g = do
+       print (toShell g)
+       v <- JSONRPC.send session $ JSONRPC.method "invoke" [toShell g]
+       case fromShell g v of
+         Nothing -> error $ "failed to parse result value " ++ show v
+         Just r -> return r
