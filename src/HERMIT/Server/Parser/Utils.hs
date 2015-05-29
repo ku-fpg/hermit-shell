ea5b359cd3aecf60d74b85390a291a53ce5263b2
Andy Gill <andygill@ku.edu>
2015-05-29 13:21:30 -0500

    Adding some deriving instance

--- src/HERMIT/Server/Parser/Utils.hs
+++ src/HERMIT/Server/Parser/Utils.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase, OverloadedStrings, FlexibleInstances, FlexibleContexts, TypeFamilies, DefaultSignatures, GADTs #-}
+{-# LANGUAGE LambdaCase, OverloadedStrings, FlexibleInstances, FlexibleContexts, TypeFamilies, DefaultSignatures, GADTs, RankNTypes, ScopedTypeVariables, StandaloneDeriving, DeriveDataTypeable #-}
 module HERMIT.Server.Parser.Utils 
         ( External(parseExternal, parseExternals)
         , external
@@ -13,7 +13,17 @@ import           Data.Aeson as Aeson
 import           Data.Aeson.Types (parseMaybe, Parser)
 import           Data.Text (Text)
 
+import           Data.Typeable
+
 import           HERMIT.External (CmdTag(..))
+
+-- For the instances: To remove for 7.10
+import          HERMIT.Context
+import          HERMIT.Core
+import          HERMIT.Kure.Universes
+import          HERMIT.Monad
+import          HERMIT.Shell.Command
+import          Language.KURE
 
 -- For now
 import           Debug.Trace
@@ -32,7 +42,7 @@ external nm f _ (Object o) = case parseMaybe p o of
                   <*> o' .: "params"
 external nm _ _ _ = fail $ "no match for " ++ show nm
 
-class External e where
+class Typeable e => External e where
   type R e :: *
   type R e = e  -- default
   
@@ -47,9 +57,13 @@ class External e where
   default matchExternal :: e -> [Value] -> Parser e
   matchExternal e [] = return e
   matchExternal _ _ = fail "wrong number of arguments"
+
+  {-# MINIMAL parseExternal | parseExternals #-}
   
 instance (External a, External b) => External (a -> b) where
   type R (a -> b) = R b
+--  typeString (Proxy :: Proxy (a -> b)) = typeString (Proxy :: Proxy a) ++ " -> " ++ typeString (Proxy :: Proxy b)
+
   parseExternal _ = error "can not parseExternal for function"
   matchExternal e (v:vs) = do
           a <- parseExternal v
@@ -61,3 +75,14 @@ infixl 3 .+
 (.+) :: b -> a -> b
 (.+) = \ b _ -> b
 
+
+-- We put *all* *hermit* and *GHC* types here. Otherwise, put beside the type
+
+deriving instance Typeable Crumb
+deriving instance Typeable HermitC
+deriving instance Typeable HermitM
+deriving instance Typeable LCore
+deriving instance Typeable LCoreTC
+deriving instance Typeable SnocPath
+deriving instance Typeable Transform                    -- KURE
+deriving instance Typeable TypedEffectH
