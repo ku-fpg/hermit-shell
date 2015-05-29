ee9cbd60be6a17acb7ff564ce1d9fc529baf5da1
Andy Gill <andygill@ku.edu>
2015-05-29 16:47:50 -0500

    Adding example of eval

--- examples/fib/FibScript.hs
+++ examples/fib/FibScript.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 import HERMIT.API
 script = do
+  -- eval "binding-of 'fib"
   setPath (bindingOf "fib")
   query (remember "orig-fib")
   rewrite (anyCall (unfoldRemembered "orig-fib"))
