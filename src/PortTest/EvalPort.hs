f3a07e9f3bae0cb7f1262d9d10056345b9567087
David Young <dyoung@ittc.ku.edu>
2015-06-02 11:30:03 -0500

    Now shows HERMIT errors and added a porting program that uses eval

---
+++ src/PortTest/EvalPort.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings, PatternGuards #-}
+
+-- TODO: Make sure this works with multi-line curly brace blocks
+
+import System.Environment
+import Data.Monoid ((<>))
+
+import Data.Char (isSpace)
+import Data.List
+
+getFileName :: IO String
+getFileName = do
+    args <- getArgs
+    case args of
+        [fileName] -> return fileName
+        _          -> error $ "Wrong number of arguments. Expected 1, got "
+                               ++ show (length args)
+
+preamble :: String
+preamble
+    = unlines
+      [ "import HERMIT.API"
+      , "script :: Shell ()"
+      , "script = do"
+      ]
+
+convert :: String -> String
+convert = unlines . map convertLine . lines
+
+convertLine :: String -> String
+convertLine ""   = ""
+convertLine line = "  eval " <> show line
+
+main :: IO ()
+main = do
+    fileName <- getFileName
+    file <- readFile fileName
+
+    putStrLn (preamble <> convert file)
