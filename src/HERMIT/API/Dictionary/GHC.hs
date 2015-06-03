2f65a347851d38e8736c6736420ebdf970595e95
RyanGlScott <ryan.gl.scott@ku.edu>
2015-06-03 13:38:27 -0500

    HERMIT.API.Dictionary.GHC

---
+++ src/HERMIT/API/Dictionary/GHC.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+module HERMIT.API.Dictionary.GHC where
+
+import Data.Aeson
+import HERMIT.API.Types
+
+-- | Deshadow a program.
+deshadowProg :: Rewrite LCore
+deshadowProg = Transform $ method "deshadowProg" []
+
+-- | Zap the occurrence information in the current identifer if it is a zombie.
+dezombify :: Rewrite LCore
+dezombify = Transform $ method "dezombify" []
+
+-- | Perform dependency analysis on all sub-expressions; simplifying and updating identifer info.
+occurrenceAnalysis :: Rewrite LCore
+occurrenceAnalysis = Transform $ method "occurrenceAnalysis" []
+
+-- | Runs GHC's Core Lint, which typechecks the current expression.
+-- Note: this can miss several things that a whole-module core lint will find.
+-- For instance, running this on the RHS of a binding, the type of the RHS will
+-- not be checked against the type of the binding. Running on the whole let expression
+-- will catch that however.
+lintExpr :: Transform LCoreTC String
+lintExpr = Transform $ method "lintExpr" []
+
+-- | Runs GHC's Core Lint, which typechecks the current module.
+lintModule :: Transform LCoreTC String
+lintModule = Transform $ method "lintModule" []
+
+-- | Lint check a clause.
+lint :: Transform LCoreTC String
+lint = Transform $ method "lint" []
+
+-- -- | Dynamically load a library of lemmas.
+-- loadLemmaLibrary :: Name -> Transform LCore String
+
+-- -- | Dynamically load a specific lemma from a library of lemmas.
+-- loadLemmaLibrary :: Name -> Name -> Transform LCore String
+
+-- | Inject a dependency on the given module.
+injectDependency :: String -> Transform LCore ()
+injectDependency str = Transform $ method "injectDependency" [toJSON str]
--- src/HERMIT/Server/Parser/Rewrite.hs
+++ src/HERMIT/Server/Parser/Rewrite.hs
@@ -79,6 +79,14 @@ instance External (RewriteH LCore) where
 --     , external "static-arg-pos" (promoteDefR . staticArgPosR :: [Int] -> RewriteH LCore)
 --         [ "perform the static argument transformation on a recursive function, only transforming the arguments specified (by index)." ]
 
+      -- HERMIT.API.Dictionary.GHC
+    , external "deshadowProg" (promoteProgR deShadowProgR :: RewriteH LCore)
+        [ "Deshadow a program." ] .+ Deep
+    , external "dezombify" (promoteExprR dezombifyR :: RewriteH LCore)
+        [ "Zap the occurrence information in the current identifer if it is a zombie."] .+ Shallow
+    , external "occurrenceAnalysis" (occurrenceAnalysisR :: RewriteH LCore)
+        [ "Perform dependency analysis on all sub-expressions; simplifying and updating identifer info."] .+ Deep
+
       -- ???
     , external "unfoldRemembered" (promoteExprR . unfoldRememberedR Obligation :: LemmaName -> RewriteH LCore)
         [ "Unfold a remembered definition." ] .+ Deep .+ Context
