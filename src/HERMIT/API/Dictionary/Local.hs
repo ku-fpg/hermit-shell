394ce7b07e0851d0d325a24f179e552891382cdd
RyanGlScott <ryan.gl.scott@ku.edu>
2015-06-04 11:07:48 -0500

    Local.Case

--- src/HERMIT/API/Dictionary/Local.hs
+++ src/HERMIT/API/Dictionary/Local.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 module HERMIT.API.Dictionary.Local
     ( module HERMIT.API.Dictionary.Local.Bind
+    , module HERMIT.API.Dictionary.Local.Case
     ) where
 
 import HERMIT.API.Dictionary.Local.Bind
+import HERMIT.API.Dictionary.Local.Case
--- src/HERMIT/API/Dictionary/Local/Bind.hs
+++ src/HERMIT/API/Dictionary/Local/Bind.hs
@@ -3,8 +3,12 @@ module HERMIT.API.Dictionary.Local.Bind where
 
 import HERMIT.API.Types
 
+-- | Convert a non-recursive binding into a recursive binding group with a single definition.
+-- NonRec v e ==> Rec [Def v e]
 nonrecToRec :: Rewrite LCore
 nonrecToRec = Transform $ method "nonrecToRec" []
 
+-- | Convert a singleton recursive binding into a non-recursive binding group.
+-- Rec [Def v e] ==> NonRec v e,  (v not free in e)
 recToNonrec :: Rewrite LCore
 recToNonrec = Transform $ method "recToNonrec" []
