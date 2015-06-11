14cbb4016107fdf40180fab61687b5774f5ddbb4
David Young <dyoung@ittc.ku.edu>
2015-06-11 15:07:43 -0500

    More examples use 'scope'

--- examples/evaluation/EvalScript.hs
+++ examples/evaluation/EvalScript.hs
@@ -7,23 +7,25 @@ script = do
   apply flattenModule
   setPath $ bindingOf "eval"
   eval "ww-result-split [| abs |] [| rep |] (ww-result-AssA-to-AssC WWA)"
-  eval "{"; sendCrumb defRhs
-  apply  letSubst
-  eval "  {"; sendCrumb letBody
-  apply $     alphaLam (Just "e")
-  sendCrumb   lamBody
-  apply $     unfoldWith "abs"
-  eval "  }"
-  eval "  {"; setPath $ rhsOf "work"
-  apply $     alphaLam (Just "e") ; sendCrumb lamBody
-  apply $     unfoldWith "rep"
-  apply $     bash
-  sendCrumb lamBody; sendCrumb lamBody -- XXX: Is this right?
-  eval "    {  consider case" ; sendCrumb (caseAlt 1) ; sendCrumb altRhs ; apply $ abstract "m"
-  eval "       consider case" ; sendCrumb (caseAlt 1) ; sendCrumb altRhs ; apply $ abstract "n"
-  eval "    }"
-  apply $   anyBU (fold "rep")
-  apply $   anyTD (forward wwResultFusion)
-  eval "  }"
-  eval "}"
+  scope $ do
+    sendCrumb defRhs
+    apply  letSubst
+
+    scope $ do
+      sendCrumb letBody
+      apply $     alphaLam (Just "e")
+      sendCrumb   lamBody
+      apply $     unfoldWith "abs"
+
+    scope $ do
+      setPath $ rhsOf "work"
+      apply $     alphaLam (Just "e") ; sendCrumb lamBody
+      apply $     unfoldWith "rep"
+      apply $     bash
+      sendCrumb lamBody; sendCrumb lamBody -- XXX: Is this right?
+      scope $ do
+        eval "consider case" ; sendCrumb (caseAlt 1) ; sendCrumb altRhs ; apply $ abstract "m"
+        eval "consider case" ; sendCrumb (caseAlt 1) ; sendCrumb altRhs ; apply $ abstract "n"
+      apply $   anyBU (fold "rep")
+      apply $   anyTD (forward wwResultFusion)
 
