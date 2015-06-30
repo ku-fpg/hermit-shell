9dee20401c79926e7a943978721a3f24bf07d644
RyanGlScott <ryan.gl.scott@ku.edu>
2015-06-30 12:56:43 -0500

    Specify +module_name in test suite

--- tests/Main.hs
+++ tests/Main.hs
@@ -18,30 +18,30 @@ rootDir, examples :: FilePath
 rootDir  = "tests"
 examples = "examples"
 
-type HermitTestArgs = (FilePath, FilePath, FilePath)
+type HermitTestArgs = (FilePath, FilePath, String, FilePath)
 
 testArgs :: [HermitTestArgs]
 testArgs =
-    [ ("concatVanishes", "Flatten.hs", "FlattenScript.hs")
-    , ("concatVanishes", "QSort.hs"  , "QSortScript.hs")
-    , ("concatVanishes", "Rev.hs"    , "RevScript.hs")
-    , ("evaluation"    , "Eval.hs"   , "EvalScript.hs")
-    , ("fib"           , "Fib.hs"    , "FibScript.hs")
-    , ("fib-tuple"     , "Fib.hs"    , "FibScript.hs")
-    , ("flatten"       , "Flatten.hs", "FlattenScript.hs")
-    , ("last"          , "Last.hs"   , "LastScript.hs")
-    , ("last"          , "Last.hs"   , "NewLastScript.hs")
-    , ("mean"          , "Mean.hs"   , "MeanScript.hs")
-    , ("nub"           , "Nub.hs"    , "NubScript.hs")
-    , ("qsort"         , "QSort.hs"  , "QSortScript.hs")
-    , ("reverse"       , "Reverse.hs", "ReverseScript.hs")
-    , ("new_reverse"   , "Reverse.hs", "ReverseScript.hs")
-    , ("fix-fusion"    , "Fusion.hs" , "FusionScript.hs")
-    , ("hanoi"         , "Hanoi.hs"  , "HanoiScript.hs")
+    [ ("concatVanishes", "Flatten.hs", "Main", "FlattenScript.hs")
+    , ("concatVanishes", "QSort.hs"  , "Main", "QSortScript.hs")
+    , ("concatVanishes", "Rev.hs"    , "Main", "RevScript.hs")
+    , ("evaluation"    , "Eval.hs"   , "Main", "EvalScript.hs")
+    , ("fib"           , "Fib.hs"    , "Main", "FibScript.hs")
+    , ("fib-tuple"     , "Fib.hs"    , "Main", "FibScript.hs")
+    , ("flatten"       , "Flatten.hs", "Main", "FlattenScript.hs")
+    , ("last"          , "Last.hs"   , "Main", "LastScript.hs")
+    , ("last"          , "Last.hs"   , "Main", "NewLastScript.hs")
+    , ("mean"          , "Mean.hs"   , "Main", "MeanScript.hs")
+    , ("nub"           , "Nub.hs"    , "Main", "NubScript.hs")
+    , ("qsort"         , "QSort.hs"  , "Main", "QSortScript.hs")
+    , ("reverse"       , "Reverse.hs", "Main", "ReverseScript.hs")
+    , ("new_reverse"   , "Reverse.hs", "Main", "ReverseScript.hs")
+    , ("fix-fusion"    , "Fusion.hs" , "Main", "FusionScript.hs")
+    , ("hanoi"         , "Hanoi.hs"  , "Main", "HanoiScript.hs")
     ]
 
 mkHermitShellTest :: HermitTestArgs -> TestTree
-mkHermitShellTest (dir, hs, script) =
+mkHermitShellTest (dir, hs, moduleName, script) =
     testCase testName runHermitShell
   where
     testName :: TestName
@@ -79,7 +79,7 @@ mkHermitShellTest (dir, hs, script) =
                                , "--"
                                , "hermit-shell"
                                , hs
-                               , "+Main"
+                               , '+':moduleName
                                , script
                                , "resume"
                                , ")"
