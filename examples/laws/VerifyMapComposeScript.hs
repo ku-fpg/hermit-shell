9e5fca4876c8efa81f0924733bdfd5db195ca09f
David Young <dyoung@ittc.ku.edu>
2015-07-01 15:06:10 -0500

    Mostly done with map laws

---
+++ examples/laws/VerifyMapComposeScript.hs
@@ -0,0 +1,70 @@
+module VerifyMapComposeScript (mapCompose) where
+
+import HERMIT.API
+
+composeLeft :: Rewrite LCore
+composeLeft
+  = serialise
+      [ oneBU (inlineWith "map")
+      , smash
+      ]
+
+nilRight :: Rewrite LCore
+nilRight
+  = serialise
+      [ pathR [appArg]
+          $ serialise
+              [ oneBU (inlineWith "map")
+              , smash
+              ]
+      , oneBU (inlineWith "map")
+      , smash
+      ]
+
+consRight :: Rewrite LCore
+consRight
+  = serialise
+      [ pathR [appArg]
+          $ serialise
+              [ oneBU (inlineWith "map")
+              , smash
+              ]
+      , oneBU (inlineWith "map")
+      , smash
+      , oneBU (lemmaBackward "ind-hyp-0")
+      , oneBU (inlineWith ".")
+      , smash
+      ]
+
+mapCompose :: Shell ()
+mapCompose = do
+  eval "rule-to-lemma \"map-compose\""
+
+  proof "map-compose" $ do
+    apply $ induction "xs"
+
+    pathS [forallBody] $ do
+        -- undefined case
+      pathS [conjLhs] $ do
+        apply . anyBU $ inlineWith "map"
+        apply smash
+
+        -- nil case
+      pathS [conjRhs, conjLhs] $ do
+        apply . rhsR $ nilRight
+        apply . lhsR $ unfoldWith "map"
+        apply smash
+
+        -- cons case
+      pathS [conjRhs, conjRhs, forallBody, consequent] $ do
+        apply . rhsR $ consRight
+        apply . oneBU $ inlineWith "map"
+        apply smash
+        pathS [eqLhs] $ do
+          apply . oneBU $ inlineWith "."
+          apply smash
+        apply reflexivity
+
+script :: Shell ()
+script = mapCompose
+
