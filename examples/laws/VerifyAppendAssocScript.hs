5aebf979354c8025cc4eb250378720a0d11d2116
David Young <dyoung@ittc.ku.edu>
2015-07-01 11:42:00 -0500

    VerifyAppendAssocScript.hs is working

--- examples/laws/VerifyAppendAssocScript.hs
+++ examples/laws/VerifyAppendAssocScript.hs
@@ -6,9 +6,51 @@ import HERMIT.API
 
 import VerifyNilAppendScript
 
+eqBoth :: Rewrite a -> Rewrite a
+eqBoth r
+  = pathR [eqLhs] r
+  >>> pathR [eqRhs] r
+
 appendAssoc :: Shell ()
 appendAssoc = do
   eval "rule-to-lemma \"append-assoc\""
+
+  nilAppend
+
+  proof "append-assoc" $ do
+    apply $ induction "x"
+
+    pathS [forallBody] $ do
+      -- undefined case
+      pathS [conjLhs] $ do
+        pathS [forallBody] $ do
+          apply . eqBoth . oneBU $ inlineWith "++"
+          apply smash
+          apply . pathR [eqRhs] . oneTD $ inlineWith "++"
+          apply smash
+
+      -- nil case
+      pathS [conjRhs, conjLhs] $ do
+        pathS [forallBody] $ do
+          apply . pathR [eqLhs] . oneBU $ inlineWith "++"
+          apply smash
+          apply . pathR [eqRhs, appFun, appArg] $ lemmaForward "nil-append"
+          apply reflexivity
+
+      -- cons case
+      pathS [conjRhs, conjRhs, forallBody, consequent] $ do
+        apply . oneTD $ inlineWith "++"
+        apply smash
+        apply . oneTD $ lemmaForward "ind-hyp-0"
+
+          -- Float (:) application out
+        pathS [eqRhs] $ do
+          apply . oneBU $ inlineWith "++"
+          apply smash
+          apply . oneBU $ inlineWith "++"
+          apply smash
+        apply reflexivity
+    apply smash  -- 'a => true' is true
 
 script :: Shell ()
 script = appendAssoc
