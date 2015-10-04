cc538e130eefc96ddb1183cae4ee6d7c5544eecb
Andy Gill <andygill@ku.edu>
2015-10-03 23:34:35 -0500

    Fixing append nonempty

--- examples/laws/VerifyAppendNonemptyScript.hs
+++ examples/laws/VerifyAppendNonemptyScript.hs
@@ -7,7 +7,7 @@ appendNonempty = do
   eval "rule-to-lemma append-nonempty"
 
   proof "append-nonempty" $ do
-    pathS [forallBody] $ do
+    pathS [forallBody,forallBody,forallBody,forallBody] $ do
       pathS [eqRhs] $ do
         apply . oneTD $ unfoldWith "++"
       apply smash
