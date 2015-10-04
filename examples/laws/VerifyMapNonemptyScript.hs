2f8fbd8d5718668f5ca3e1e9db9b14ce350193ea
Andy Gill <andygill@ku.edu>
2015-10-03 23:36:48 -0500

    Adding map nonempty

--- examples/laws/VerifyMapNonemptyScript.hs
+++ examples/laws/VerifyMapNonemptyScript.hs
@@ -7,7 +7,7 @@ mapNonempty = do
   eval "rule-to-lemma map-nonempty"
 
   proof "map-nonempty" $ do
-    pathS [forallBody] $ do
+    pathS [forallBody,forallBody,forallBody,forallBody] $ do
       apply . lhsR $ unfoldWith "map"
       apply smash
 
